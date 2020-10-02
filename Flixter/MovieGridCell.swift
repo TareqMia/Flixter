b7c4d93b21825e49db09403c5b3ec45ed6e52572
Tareq Mia <tareqmia81@gmail.com>
2020-10-02 18:09:43 -0400

    Flixter Part 2

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Tareq Mia on 10/2/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+}
